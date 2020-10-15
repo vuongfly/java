7cd92224d4ff75506c9ff9f0b29d7ed751305f5a
vuongfly <68699694+vuongfly@users.noreply.github.com>
2020-10-15 21:13:06 +0700

    add id for user

--- BankSystem/model/User.java
+++ BankSystem/model/User.java
@@ -1,11 +1,20 @@
 package model;
 
 public class User {
+    String id;
     String name;
     Account account;
     Card card;
 
     public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public Account getAccount() {
