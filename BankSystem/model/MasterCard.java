7cef3a45131e60934de322ed1ebeb02a9d97373d
vuongfly <68699694+vuongfly@users.noreply.github.com>
2020-10-15 21:34:06 +0700

    add function for master card and visa card

--- BankSystem/model/MasterCard.java
+++ BankSystem/model/MasterCard.java
@@ -1,7 +1,21 @@
 package model;
 
 public class MasterCard extends Card {
+    public double interestRate = 0.03;
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
     public MasterCard() {
         super();
+    }
+
+    public void monthlyInterest() {
+        this.balance += this.balance * this.interestRate;
     }
 }
--- BankSystem/model/Visa.java
+++ BankSystem/model/Visa.java
@@ -1,8 +1,25 @@
 package model;
 
-public class Visa extends Card{
+public class Visa extends Card {
+
+    public double debtLimit;
 
     public Visa() {
         super();
+    }
+
+    @Override
+    public boolean transferFund(Card cardReceive, double amount) {
+        System.out.println("Visa card cannot transfer");
+        return false;
+    }
+
+    @Override
+    public boolean withdrawFund(double amount) {
+        if (this.debtLimit + debtLimit > amount) {
+            return super.withdrawFund(amount);
+        } else {
+            return false;
+        }
     }
 }
