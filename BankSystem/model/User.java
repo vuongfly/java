c8e1584cf63b73f2b6fd7556e95f2c692577b860
vuongfly <68699694+vuongfly@users.noreply.github.com>
2020-10-15 21:10:12 +0700

    fix user class

--- BankSystem/model/User.java
+++ BankSystem/model/User.java
@@ -8,6 +8,14 @@ public class User {
     public User() {
     }
 
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
     public User(String name) {
         this.name = name;
     }
